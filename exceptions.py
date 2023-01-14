c6ff67a3f7288e6e2df9d0dbd588b2e46beef7b8
Аким Матвиенко <akimmatvienko@rambler.ru>
2023-01-14 22:42:34 +0300

    Спринт 7. Первое ревью

---
+++ exceptions.py
@@ -0,0 +1,28 @@
+class NoneEnvVariableException(Exception):
+    """Ошибка незаполненной переменной окружения."""
+
+    pass
+
+
+class ApiRequestException(Exception):
+    """Ошибка при обращении к API."""
+
+    pass
+
+
+class NotOkStatusCodeException(Exception):
+    """Ошибка, статус код не 200."""
+
+    pass
+
+
+class HomeWorkApiException(Exception):
+    """Ошибка при неправильном наполнении словаря homework в ответе API."""
+
+    pass
+
+
+class InvalidTelegramTokenException(Exception):
+    """Ошибка при некорректном токене для бота Telegram."""
+
+    pass
